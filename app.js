62191794451d2eb217573175992ab83f50ba8c55
ameralshabebi <69112877+ameralshabebi@users.noreply.github.com>
2022-06-26 19:06:55 +0300

    add fetch also test it

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 import express from 'express';
+import fetch from 'node-fetch';
 
 const app = express();
+app.use(express.static('./public'));
 
-app.get('/', (req, res) => {
+app.get('/user', (req, res) => {
+	fetch('https://dummyapi.io/data/v1/user?limit=10', {
+		headers: {
+			'app-id': '62b87ff2698f406b46f32372',
+		},
+	})
+		.then((data) => data.json())
+		.then((data) => console.log(data));
+
 	res.send('ecwid integration');
 });
 app.listen(8000, () => {
 	console.log('app is running on 5000');
 });
-
-console.log('version2');
