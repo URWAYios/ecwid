38381a3498d7a453f474bddcc3e2e38aaf7a6622
ameralshabebi <69112877+ameralshabebi@users.noreply.github.com>
2022-06-26 16:50:22 +0300

    initial setup

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('ecwid integration');
+});
+app.listen(8000, () => {
+	console.log('app is running on 5000');
+});
+
+console.log('version1');
