37caffd7e5501e544a07a4b7d737880b29910a0f
ameralshabebi <69112877+ameralshabebi@users.noreply.github.com>
2022-06-29 20:53:46 +0300

    adding env variable

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ import express from 'express';
 import fetch from 'node-fetch';
 import reqeustpayment from './routes/reqeustpayment.js';
 import cors from 'cors';
+import 'dotenv/config';
 
+const PORT = process.env.PORT || 8000;
 const corsOptions = {
 	origin: '*',
 	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
 const app = express();
+
 app.use(express.static('./public'));
 app.use(cors(corsOptions));
 app.use('/v1/urway/ecwid', reqeustpayment);
-app.listen(8000, () => {
-	console.log('app is running on 8000');
+
+app.listen(PORT, () => {
+	console.log('app is running on ' + PORT);
+	console.log(process.env.TEST + '.....' + process.env.LIVE);
 });
