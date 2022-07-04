35b79af7863a0c007c37417db22e65f59e6bb41c
ameralshabebi <69112877+ameralshabebi@users.noreply.github.com>
2022-07-04 08:30:52 +0300

    slight changes 45

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('./public'));
-app.use(cookieParser());
+app.use(cookie());
 app.use(cors(corsOptions));
 app.use(cookieHelper());
 app.use('/v1/urway/ecwid', reqeustpayment);
